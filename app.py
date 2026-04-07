cdf022faf818166d871b4b44ac5351f7b828e932
Kassidy Flick <kassidy.flick@colorado.edu>
2026-04-07 18:25:53 +0000

    Database Connection

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Kassidy Flick in 3308'
+
+@app.route('/dbtest')
+def testing():
+    conn = psycopg2.connect("postgresql://bballdb_d19c_user:ebReQb5RR3oxhiVHYbAo4Dl20rN0qkZj@dpg-d7akms6slomc73dva49g-a/bballdb_d19c")
+
+    conn.close()
+    return "Database Connection Successful"
