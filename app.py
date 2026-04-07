66a943ac218b70506fdf6a2f21f162bcba1c75b4
Kassidy Flick <kassidy.flick@colorado.edu>
2026-04-07 19:03:59 +0000

    Create, Insert, and Select

--- app.py
+++ app.py
@@ -7,9 +7,63 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Kassidy Flick in 3308'
 
-@app.route('/dbtest')
+@app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgresql://bballdb_d19c_user:ebReQb5RR3oxhiVHYbAo4Dl20rN0qkZj@dpg-d7akms6slomc73dva49g-a/bballdb_d19c")
 
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://bballdb_d19c_user:ebReQb5RR3oxhiVHYbAo4Dl20rN0qkZj@dpg-d7akms6slomc73dva49g-a/bballdb_d19c")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://bballdb_d19c_user:ebReQb5RR3oxhiVHYbAo4Dl20rN0qkZj@dpg-d7akms6slomc73dva49g-a/bballdb_d19c")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Suxxessfully Populated"
+
+app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://bballdb_d19c_user:ebReQb5RR3oxhiVHYbAo4Dl20rN0qkZj@dpg-d7akms6slomc73dva49g-a/bballdb_d19c")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string= ""
+    response_string +="<table>"
+
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
+
